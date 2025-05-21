e72da0f52e941c4e78cd2537fb08eef34c86e8b1
nahashon <nashonmwendwa0@gmail.com>
2025-05-21 14:56:24 +0300

    define the account  model

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Account extends Model
 {
-    //
+    use HasFactory;
+   
+    //The attributes that are mass assignable via create() or update()
+    protected $fillable =[
+        'account_number',
+        'holder_name',
+        'balance',
+        'account_type',
+    ];
 }
