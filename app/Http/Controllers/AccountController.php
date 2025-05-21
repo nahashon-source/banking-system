6afb9d0c2e7dcbdb9501df04e882323afe0f2e51
nahashon <nashonmwendwa0@gmail.com>
2025-05-21 15:02:45 +0300

    add method to list all accounts

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
-    //
+    //Return all accounts as JSON
+    public function index()
+    {
+        $accounts = Account::all();
+        return response()->json($accounts);
+    }
 }
